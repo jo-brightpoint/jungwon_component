f81b8a012ae64e8a00c4675929224ee09e40a761
Jungwon Oh <jungwon@brightpoint.net>
2020-02-18 14:27:11 -0800

    style added

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -5,9 +5,11 @@ import quote1 from '../images/quote1.svg';
 import quote2 from '../images/quote2.svg';
 import quote1_1 from '../images/quote1-1.png';
 import quote2_1 from '../images/quote2-1.png';
-import testimonial1 from '../images/testimonial01.png';
-import testimonial2 from '../images/testimonial02.png';
-import testimonial3 from '../images/testimonial03.png';
+import testimonial1 from '../images/testimonial01.jpg';
+import testimonial2 from '../images/testimonial02.jpg';
+import testimonial3 from '../images/testimonial03.jpg';
+import {BrowserView, MobileView} from 'react-device-detect';
+import Slider from 'react-slick';
 
 class Mainpage extends React.Component{
     
@@ -52,14 +54,18 @@ class Mainpage extends React.Component{
     displayImages=()=>{
         return this.state.testimonials.map((testimonial)=>{
             if(testimonial.id === 1){
-                return (<img key={testimonial.id} src={testimonial.profileImage} alt=""className={"opacity-100 mx-6 rounded-full w-auto border-white border-2"}/>);  
+                return (<img 
+                            key={testimonial.id} 
+                            src={testimonial.profileImage} 
+                            alt="testimonial profile"
+                            className={"opacity-100 h-auto rounded-full border-white border-2 bg-white lg:w-auto w-2/6 lg:mx-4"}/>);  
             }
             else{
                 return (<img 
                 key={testimonial.id} 
                 src={testimonial.profileImage} 
-                alt=""
-                className={"opacity-25 cursor-pointer"} 
+                alt="testimonial profile"
+                className={"opacity-25 cursor-pointer rounded-full lg:w-auto w-2/6"} 
                 onClick={(e) => this.imageClickHandler(e, testimonial)}
                 />
                 )}
@@ -95,54 +101,100 @@ class Mainpage extends React.Component{
     }
     
     render(){
+        const settings = {
+            dots: true,
+            intinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+        }
         return(
             <>
-            <div className="flex justify-center mt-12 mx-12 px-12"> 
-                <div className="" style={{zIndex: "-1"}}>
-                    <img src={phone} alt="phone" className="max-w-xs"/>
-                </div>
-                <div className="flex flex-col ml-12 pt-20 px-8 leading-tight" style={{maxWidth: "35rem"}}>
-                    <h2 className="text-5xl font-bold ">Funding at the speed of opportunity</h2>
-                    <p className="my-8 text-bpGray text-lg" style={{color: "#757e8c"}}> With fast, reliable funding through BrightPoint, 
+            <div className="flex text-center justify-center flex-col mt-4 lg:text-left lg:flex-row-reverse lg:mt-12 lg:mx-12 lg:px-12"> 
+                <div className="flex flex-col leading-tight px-6 lg:ml-12 lg:pt-20 lg:px-8" style={{maxWidth: "35rem"}}>
+                    <h2 className="font-bold text-3xl text-left lg:text-5xl">Funding at the speed of opportunity</h2>
+                    <p className="text-bpGray lg:text-lg lg:my-8 lg:flex hidden" style={{color: "#757e8c"}}> With fast, reliable funding through BrightPoint, 
                         get the flexibility you need to take advantage of
                         whatever opportunities come your way.
                     </p>
                     <button 
-                        className="hover:bg-blue-700 text-white font-bold py-2 px-4 h-10 rounded-full max-w-1/3"
-                        style={{backgroundColor: "#377ef9", maxWidth: "33%"}}
+                        className="hover:bg-blue-700 text-white font-bold max-w-1/2 h-12 rounded-full mt-4 lg:mt-0 lg:py-2 lg:px-4 lg:h-10 lg:max-w-1/3"
+                        style={{backgroundColor: "#377ef9"}}
                     >
                         Apply now
                     </button>
-                    <p className="mt-6 text-gray-600 text-base">Applying will not affect  your credit score.</p>
+                    <p className="mt-6 text-left text-gray-600 text-base">Applying will not affect  your credit score.</p>
+                </div>
+                <div className="mt-10 lg:mt-0" style={{zIndex: "-1"}}>
+                    <img src={phone} alt="phone" className="max-w-xs pr-8 lg:pr-0"/>
                 </div>
             </div>
 
             <div 
-                className="w-100 h-auto flex flex-col justify-center text-center py-20 px-40 -mt-64" 
+                className="w-100 h-auto flex flex-col justify-center text-center py-10 -mt-64 lg:py-20 lg:px-40 lg:-mt-64" 
                 style={{backgroundColor:"#1e344c"}} >
-                <h3 className="text-center text-5xl font-bold text-white mb-10">Meet some of our incredible customers</h3>
-                <div className="flex justify-center">
-                    {this.displayImages()}
-                </div>
-                <div className="flex justify-center my-4 max-w-3xl ml-auto mr-auto mt-10">
-                    {/* <p>{this.state.testimonials.filter(t=>t.id === this.state.selectedIndex)[0].words}</p> */}
-                    <div className="mb-auto w-1/2 mr-4 -mt-2" style={{maxWidth: "2rem"}}><img src={quote1_1} alt=""/></div>
-                    <div className="w-1/2">
-                        <p className="text-white text-left" id='testimonial-text'>{this.state.testimonials[1].words}</p>
-                        <br />
-                        <p className="text-white text-left" id="testimonial-info">{this.state.testimonials[1].info}</p>
+                <h3 className="justify-center text-3xl font-bold text-white mb-10 text-center lg:text-5xl">Meet some of our incredible customers</h3>
+                
+                <BrowserView>
+                    <div className="flex justify-center w-full h-auto">
+                        {this.displayImages()}
                     </div>
-                    <div className="mt-auto w-1/2" style={{maxWidth: "2rem"}}><img src={quote2_1} alt=""/></div>
-                </div>
+                    <div className="flex justify-center my-4 max-w-3xl mx-auto mt-10">
+                        <div className="mb-auto w-1/2 mr-4 -mt-2" style={{maxWidth: "2rem"}}><img src={quote1_1} alt=""/></div>
+                        <div className="w-1/2">
+                            <p className="text-white text-left" id='testimonial-text'>{this.state.testimonials[1].words}</p>
+                            <br />
+                            <p className="text-white text-left" id="testimonial-info">{this.state.testimonials[1].info}</p>
+                        </div>
+                        <div className="mt-auto w-1/2" style={{maxWidth: "2rem"}}><img src={quote2_1} alt=""/></div>
+                    </div>
+                </BrowserView>
+                <MobileView>
+                    <Slider {...settings}>
+                        <div className="flex flex-col justify-center text-center">
+                            <img 
+                            key={this.state.testimonials[0].id} 
+                            src={this.state.testimonials[0].profileImage} 
+                            alt="testimonial profile"
+                            className={"opacity-100 h-auto rounded-full w-2/6 border-white border-2 mx-auto mb-4"}/>
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[0].words}</p>
+                            <br />
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[0].info}</p>
+                        </div>
+
+                        <div className="flex flex-col justify-center text-center">
+                            <img 
+                                key={this.state.testimonials[1].id} 
+                                src={this.state.testimonials[1].profileImage} 
+                                alt="testimonial profile"
+                                className={"opacity-100 h-auto rounded-full w-2/6 border-white border-2 mx-auto mb-4"}/>
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[1].words}</p>
+                            <br />
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[1].info}</p>
+                        </div>
+                        
+                        <div className="flex flex-col justify-center text-center">
+                            <img 
+                                key={this.state.testimonials[2].id} 
+                                src={this.state.testimonials[2].profileImage} 
+                                alt="testimonial profile"
+                                className={"opacity-100 h-auto rounded-full w-2/6 border-white border-2 mx-auto mb-4"}/>
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[2].words}</p>
+                            <br />
+                            <p className="text-left text-gray-500 px-8">{this.state.testimonials[2].info}</p>
+                        </div>
+                    </Slider>
+                </MobileView>
 
             </div>
 
-            <div className="flex flex-col justify-center items-center my-24">
-                <h3 className="text-5xl font-bold mb-6">Speak to our loan specialist</h3>
-                <h2 className="text-4xl font-bold mb-6 h-14" style={{color: "#377ef9"}}> 
+            <div className="flex flex-col justify-center items-center leading-tight my-12 lg:my-24">
+                <h3 className="font-bold text-3xl text-center mx-auto mb-6 px-16 lg:text-5xl">Speak to our loan specialist</h3>
+                <h2 className="font-bold text-3xl h-14 mb-4 lg:text-4xl lg:mb-6" style={{color: "#377ef9"}}> 
                     (888) 567-8912
                 </h2>
-                <p className="text-2xl text-gray-600">Monday - Friday  |  7am - 4pm PST</p>
+                <p className="lg:text-2xl text-xl text-gray-600">Monday - Friday  |  7am - 4pm PST</p>
                 <button 
                     className="hover:bg-blue-700 text-white font-bold px-10 h-12 rounded-full mt-10"
                     style={{backgroundColor: "#377ef9"}}
@@ -158,34 +210,3 @@ class Mainpage extends React.Component{
 
 export default Mainpage;
 
-// module.exports = {
-//     important: true,
-//     theme: {
-//       extend: {},
-//       maxWidth: {
-//         '1/4': '25%',
-//         '1/2': '50%',
-//         '3/4': '75%',
-//       },
-//       colors: {
-//         bpBlue:{
-//           primary: ''
-//         }
-//       },
-//       backgroundColor: theme => ({
-//         'primary': '#377ef9',
-//       }),
-//       textColor: {
-//         'bpGray': '#757e8c',
-//         'bpNavy': '#1e344c',
-//         // 'secondary': '#ffed4a',
-//         // 'danger': '#e3342f',
-//       }
-//     },
-    
-//     variants: {
-//       opacity: ['responsive', 'hover']
-//     },
-//     plugins: [],
-//   }
-  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 module.exports = {
-  important: true,
   theme: {
-    backgroundColor: theme =>({
-      'bpPrimary' : '#377ef9'
-    }),
     extend: {
       colors:{
         bpBlue: '#377ef9'
+      },
+      fontFamily: {
+        sans: [
+          "Messina",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ],
+        sansBold: [
+          "Messina Bold",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ],
+        serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+        mono: ["Menlo", "Monaco", "Consolas", '"Liberation Mono"', '"Courier New"', "monospace"]
       }
     },
     maxWidth:{
